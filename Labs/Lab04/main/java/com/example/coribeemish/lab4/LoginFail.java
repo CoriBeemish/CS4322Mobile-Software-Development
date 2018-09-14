98e5ae1d44e0a51634f27c419494c0a9ee24d639
CoriBeemish <cbeemish@gmail.com>
2018-09-13 20:02:45 -0400

    Update LoginFail.java

--- Labs/Lab04/main/java/com/example/coribeemish/lab4/LoginFail.java
+++ Labs/Lab04/main/java/com/example/coribeemish/lab4/LoginFail.java
@@ -13,14 +13,15 @@ import android.widget.TextView;
 
 public class LoginFail extends AppCompatActivity {
 
-    TextView loginFailMessage;
-    Button backButtonFail;
+    private TextView loginFailMessage;
+    private Button backButtonFail;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login_fail);
 
+        //get references to the TextView Labels
         loginFailMessage = (TextView) findViewById(R.id.loginFailMessage);
         loginFailMessage.setText("Login Fail! Please go back and try again!");
 
@@ -30,6 +31,7 @@ public class LoginFail extends AppCompatActivity {
         backButtonFail.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                //Sends the user back to the login page
                 Intent intent = new Intent(getBaseContext(), Login.class);
                 startActivity(intent);
             }
